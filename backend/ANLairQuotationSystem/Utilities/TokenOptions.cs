ccf58a64148c764a8714ca2fb7265da2c5217771
Toni <ponsadvento08@gmail.com>
2026-08-25 21:57:07 +0800

    Feat: Centralized refresh token options and details

---
+++ backend/ANLairQuotationSystem/Utilities/TokenOptions.cs
@@ -0,0 +1,28 @@
+﻿namespace ANLairQuotationSystem.Utilities;
+
+public class TokenOptions(
+        IConfiguration configuration
+    )
+{
+    private readonly IConfiguration _configuration = configuration;
+
+    public string RefreshCookieName => _configuration["UtilValues:RefreshTokenCookieName"]!;
+
+    public CookieOptions RefreshTokenOptions => new()
+    {
+        HttpOnly = true,
+        Secure = true,
+        SameSite = SameSiteMode.Lax,
+        Expires = DateTime.Now.AddDays(double.Parse(_configuration["JwtSettings:RefreshTokenExpiryInDays"]!)),
+        Path = "/api/auth"
+    };
+
+    public CookieOptions ExpiredRefreshTokenOptions => new()
+    {
+        HttpOnly = true,
+        Secure = true,
+        SameSite = SameSiteMode.Lax,
+        Expires = DateTime.Now.AddDays(-1),
+        Path = "/api/auth"
+    };
+}
