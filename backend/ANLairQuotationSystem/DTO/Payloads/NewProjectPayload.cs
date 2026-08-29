e64af73d03d9056f6287e8cc9961361f2cdc6d26
Toni <ponsadvento08@gmail.com>
2026-08-29 23:02:16 +0800

    Feat: Project payload for creating new projects

---
+++ backend/ANLairQuotationSystem/DTO/Payloads/NewProjectPayload.cs
@@ -0,0 +1,20 @@
+﻿using static ANLairQuotationSystem.Entities.Project;
+
+namespace ANLairQuotationSystem.DTO.Payloads;
+
+public class NewProjectPayload
+{
+    public required string UserPublicId { get; set; }
+    public required string ClientPublicId { get; set; }
+    public required string Name { get; set; }
+    public DateTime? DateRequested { get; set; } = null;
+    public required string RequestorFirstname { get; set; }
+    public string? RequestorMiddlename { get; set; } = null;
+    public required string RequestorSurname { get; set; }
+    public string? RequestorExtensionName { get; set; } = null;
+    public required string RequestorPosition { get; set; }
+    public required string HospitalName { get; set; }
+    public ProjectStatus Status { get; set; }
+    public List<string>? ItemTemplateUniqueId { get; set; } = null;
+
+}
