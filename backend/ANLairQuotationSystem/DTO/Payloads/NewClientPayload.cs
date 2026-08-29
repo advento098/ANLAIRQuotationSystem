3cc128317f53504c4a24240d7628b27313ed2792
Toni <ponsadvento08@gmail.com>
2026-08-29 23:01:29 +0800

    Feat: Client payload for creatyin n

---
+++ backend/ANLairQuotationSystem/DTO/Payloads/NewClientPayload.cs
@@ -0,0 +1,15 @@
+﻿namespace ANLairQuotationSystem.DTO.Payloads;
+
+public class NewClientPayload
+{
+    public required string CreatorPublicId { get; set; }
+    public string? CompanyName { get; set; }
+    public required string Firstname { get; set; }
+    public string? Middlename { get; set; }
+    public required string Surname { get; set; }
+    public string? ExtensionName { get; set; }
+    public string? Position { get; set; }
+    public string? ContactNumber { get; set; }
+    public required string Email { get; set; }
+    public required string Address { get; set; }
+}
