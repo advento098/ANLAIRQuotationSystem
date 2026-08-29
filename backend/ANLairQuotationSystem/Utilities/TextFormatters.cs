87b2862e6d3c41b0c7e8ce6efff03e8bc8a97d68
Toni <ponsadvento08@gmail.com>
2026-08-29 23:02:37 +0800

    Feat: Text formatters, text to title case

---
+++ backend/ANLairQuotationSystem/Utilities/TextFormatters.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace ANLairQuotationSystem.Utilities;
+
+public class TextFormatters
+{
+    public static string ToTitleCase(string? text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+            return string.Empty;
+
+        return CultureInfo.CurrentCulture.TextInfo
+            .ToTitleCase(text.Trim().ToLower());
+    }
+}
