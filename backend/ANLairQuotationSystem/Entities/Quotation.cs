71d8c88362074168fcc92f7e0a8dc62745d27bb9
Toni <ponsadvento08@gmail.com>
2026-08-31 21:43:40 +0800

    Feat: Business rule on quotation entity that finalizes the

--- backend/ANLairQuotationSystem/Entities/Quotation.cs
+++ backend/ANLairQuotationSystem/Entities/Quotation.cs
@@ -3,10 +3,24 @@
 public class Quotation
 {
     public uint ProjectId { get; set; }
-    public decimal ItemsFinalCost { get; set; }
-    public decimal FinalCost { get; set; }
+    public decimal ItemsFinalCost { get; private set; }
+    public decimal FinalCost { get; private set; }
 
     public Project Project { get; set; } = null!;
     public ICollection<QuotationAdditional> QuotationAdditionals { get; set; } = [];
     public ICollection<QuotationComputationConstant> QuotationComputationConstants { get; set; } = [];
+
+    /// <summary>
+    /// Sets the value of the final cost based on existing data
+    /// </summary>
+    public void CalculateFinalCost()
+    {
+        decimal itemExpenseTotal = Project.ProjectItems.Sum(pt => pt.CalculateExpenses());
+        decimal quotationAdditionalsTotal = QuotationAdditionals.Sum(qa => qa.Cost);
+        // TODO: Continue working on final quotation cost
+        // Compute constants here before assigning to final cost
+
+        ItemsFinalCost = itemExpenseTotal;
+        //FinalCost = Math.Max(0, quotationAdditionalsTotal + itemExpenseTotal);
+    }
 }
