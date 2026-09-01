b47252926845c52a63877247d39f8505799d8204
Toni <ponsadvento08@gmail.com>
2026-09-01 20:49:08 +0800

    Feat: Added operator property for quotation additionals

--- backend/ANLairQuotationSystem/Entities/QuotationAdditional.cs
+++ backend/ANLairQuotationSystem/Entities/QuotationAdditional.cs
@@ -6,9 +6,19 @@ public class QuotationAdditional
     public uint QuotationId { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
+    public QuotationOperator Operator { get; set; }
     public decimal Cost { get; set; }
     public DateTime DateCreated { get; set; }
     public DateTime DateModified { get; set; }
 
     public Quotation Quotation { get; set; } = null!;
+
+    public enum QuotationOperator
+    {
+        Add = 1,
+        Subtract = 2,
+        Multiply = 3,
+        Divide = 4,
+        PercentageAdd = 5
+    }
 }
